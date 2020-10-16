249e79e5b6893c11ccb64c8453ba6aa3a645c915
Chris Ackerland <69921541+ChrisAckerland@users.noreply.github.com>
2020-10-16 14:41:10 -0700

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,60 @@
-//pokemonList array
+let pokemonRepository = (function () {
 let pokemonList = [
   {
     name: "Seel",
-    height: 3.07,
-    weight: 198.4,
+
     category: "seaLion",
-    abilities: ["thickFat", "hydration"],
-    types: ["water"],
-    weaknesses: ["grass", "electric"],
+    height: 3.07,
   },
   {
     name: "Mamoswine",
-    height: 8.02,
-    weight: 641.5,
+
     category: "twinTusk",
-    abilities: ["oblivious", "snowCloak"],
-    types: ["ice", "ground"],
-    weaknesses: ["steel", "fire", "grass", "water", "fighting"],
+    height: 8.02,
   },
   {
     name: "Whiscash",
-    height: 2.11,
-    weight: 52.0,
+
     category: "whiskers",
-    abilities: ["oblivious", "anticipation"],
-    types: ["water", "ground"],
-    weaknesses: ["grass"],
+    height: 2.11,
   },
   {
     name: "Camerupt",
-    height: 6.03,
-    weight: 485.0,
+    
     category: "eruption",
-    abilities: ["magmaArmor", "solidRock"],
-    types: ["fire", "ground"],
-    weaknesses: ["water", "ground"],
-  },
+    height: 6.03,
+    },
   {
     name: "Slaking",
-    height: 6.07,
-    weight: 287.7,
+
     category: "lazy",
-    abilities: ["truant"],
-    types: ["normal"],
-    weaknesses: ["fighting"],
+    height: 6.07,
   },
   {
     name: "Poochyena",
-    height: 1.08,
-    weight: 30.0,
     category: "bite",
-    abilities: ["runAway", "quickFeet"],
-    types: ["dark"],
-    weaknesses: ["fairy", "bug", "fighting"],
+    height: 1.08,
+
   },
   {
     name: "Rillaboom",
-    height: 6.11,
-    weight: 198.4,
     category: "drummer",
-    abilities: ["overgrow"],
-    types: ["grass"],
-    weaknesses: ["fire", "flying", "ice", "poison", "bug"],
+    height: 6.11,
+
   }
 ];
-//For loop iterating each item in pokemonList
-// for (let i = 0; i < pokemonList.length; i++) {
-//   document.write(pokemonList[i].name);
-// if (pokemonList[i].height >=6){
-//     document.write(" - (height: " + pokemonList[i].height + ") - wow, that's big!</br>");
-//   }else if (pokemonList[i].height <=3){
-//   document.write(" - (height: " + pokemonList[i].height + ") - that's small!</br>");
-//   }else {
-//   document.write(" - (height: " + pokemonList[i].height + ") - pretty average.</br>");
-//   }
-// }
-
-pokemonList.forEach(function(pokemon){
-  document.write(pokemon.name + " (height " + pokemon.height + ")" + "<br>");
+function add(pokemon) {
+pokemonList.push(pokemon);
+}
+function getAll() {
+  return pokemonList;
+}
+return {
+  add: add,
+  getAll: getAll,
+};
+})();
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name + "- category: " + pokemon.category + "-" + " (height " + pokemon.height + ")" + "<br>");
 });
 
